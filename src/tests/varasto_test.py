bef23d96fdda6b4847d4d3b6b2bf61be9c29178d
Antti Hartikka <antti.hartikka@helsinki.fi>
2022-11-01 17:45:39 +0200

    broken

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -13,6 +13,15 @@ class TestVarasto(unittest.TestCase):
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
+    def test_uuden_varaston_negatiivinen_tilavuus_ja_saldo(self):
+        neg_varasto = Varasto(-1, -1)
+        self.assertAlmostEqual(neg_varasto.tilavuus, 0)
+        self.assertAlmostEqual(neg_varasto.saldo, 0)
+
+    def test_uuden_varaston_ylitaytto(self):
+        full_varasto = Varasto(10, 20)
+        self.assertAlmostEqual(full_varasto.saldo, full_varasto.tilavuus)
+
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -38,3 +47,27 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_negatiivinen_lisaaminen(self):
+        self.varasto.lisaa_varastoon(1)
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 1)
+
+    def test_varaston_ylitaytto(self):
+        self.varasto.lisaa_varastoon(20)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_negatiivinen_otto(self):
+        self.varasto.lisaa_varastoon(10)
+        saatu = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(saatu, 0)
+
+    def test_ylisuuri_otto(self):
+        self.varasto.lisaa_varastoon(10)
+        saatu = self.varasto.ota_varastosta(20)
+        self.assertAlmostEqual(saatu, 10)
+
+    def test_default_str(self):
+        self.assertEqual(self.varasto.__str__(), 'saldo = 0, vielä tilaa 10')
+
+
